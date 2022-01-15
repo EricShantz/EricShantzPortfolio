cd4ca438a35675ec87ab34ef0b193d4e9178610d
Eric Shantz <shan0255@algonquinlive.com>
2022-01-15 17:47:40 -0500

    add code base for testing

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let scrollDirection = true
+
+const App = {
+  init(){
+    App.addEventListeners()
+    if(document.body.id == "homePage"){
+      App.checkForSpline()
+      
+    }
+  },
+
+  addEventListeners(){
+    if(document.getElementById('portfolioHeader'))
+    document.getElementById('downArrowDiv').addEventListener('click', App.scrollDown)
+},
+
+  scrollDown(){
+    if(scrollDirection){
+    window.scrollTo({ 
+      top:695,
+    behavior:'smooth'});
+    document.getElementById('downArrow').innerHTML = "keyboard_arrow_up"
+    scrollDirection = !scrollDirection
+    }else{
+      window.scrollTo({ 
+        top:0,
+      behavior:'smooth'});
+    document.getElementById('downArrow').innerHTML = "keyboard_arrow_down"
+    scrollDirection = !scrollDirection
+    }
+  },
+
+  checkForSpline(){
+    let splineInterval = setInterval(() => {
+      if(document.readyState === 'complete'){
+        App.terminateSpinner()
+        clearInterval(splineInterval)
+      }
+    }, 500);
+  },
+
+  terminateSpinner(){
+  document.getElementById("spinner").setAttribute("class" , "loader hide")
+  },
+
+
+}
+document.addEventListener('DOMContentLoaded', App.init)
